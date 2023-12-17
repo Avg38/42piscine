4db24918db2b8040eca6047aa12052aafa1a5961
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:11:19 +0100

    Update 02_ft_putnbr.c

--- C04/02_ft_putnbr.c
+++ C04/02_ft_putnbr.c
@@ -9,25 +9,27 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+
 #include <unistd.h>
 
-void ft_putchar(char c){
-    write(1,&c,1);
-}
-void    ft_putnbr(int nb){
-    if(nb<0){
-        write(1,"-",1);
-        nb = -nb;
-    }
-    if(nb> 9){
-        ft_putnbr(nb/10);
-    }
-    ft_putchar(nb%10 + '0');
-}
-
-int main(){
-    int nb = 422;
-    ft_putnbr(nb);
-    return 0;
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11); 
+	else if (nb < 0)
+	{
+		nb = -nb;
+		write(1, "-", 1);
+		ft_putnbr(nb);
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb <= 9 && nb >= 0)
+	{
+		nb += 48;
+		write(1, &nb, 1);
+	}
 }
