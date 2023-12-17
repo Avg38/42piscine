03ff3c4d00fee8e3407cdcff84dba0e04e5f1e93
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:06:55 +0100

    Update 01_ft_recursive_factorial.c

--- C05/01_ft_recursive_factorial.c
+++ C05/01_ft_recursive_factorial.c
@@ -9,30 +9,16 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
-#include <unistd.h>
 
-int run(int i, int res, int nb){
-    if (i<=nb)
-        res = run(i+1,res,nb) * i;
-    return res;
-}
-
-int ft_recursive_factorial(int nb){
-    int i;
-    int res;
-
-    i = 1;
-    res = 1;
-    if (nb<0)
-        return 0;    
-    res = run(i,res,nb);
-    return res;
-    
-}
-
-int main(){
-    int res = ft_recursive_factorial(7);
-    printf("%d",res);
-    return 0;
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	if (nb <= 2)
+		return (nb);
+	return (ft_recursive_factorial(nb - 1) * nb);
 }
