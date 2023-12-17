2e1418e72034a40633d594f666c1097a8432e259
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:07:32 +0100

    Update 03_ft_recursive_power.c

--- C05/03_ft_recursive_power.c
+++ C05/03_ft_recursive_power.c
@@ -9,32 +9,14 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
 
-int run(int i, int res, int nb, int power){
-    if (i<power)
-        res = run(i+1,res,nb,power) * nb;
-    return res;
-    
-}
-
-int ft_recursive_power(int nb, int power){
-    int i;
-    int res;
-
-    i = 1;
-    res = nb;
-    if (power == 0)
-        return 1;
-    if (power < 0)
-        return 0;
-    res = run(i, res, nb, power);
-    return res;
-}
-
-int main(){
-    int res = ft_recursive_power(5,3);
-    printf("%d",res);
-    return 0;
+int	ft_recursive_power(int nb, int power)
+{
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (nb);
+	if (power < 0)
+		return (0);
+	return (nb * ft_recursive_power(nb, power - 1));
 }
