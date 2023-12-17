9a0e1c9487ce5bb87ab301a0a74291ccefd75878
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:16:01 +0100

    Update 06_ft_print_comb2.c

--- C00/06_ft_print_comb2.c
+++ C00/06_ft_print_comb2.c
@@ -1,46 +1,43 @@
-//42
-//06_ft_print_comb2
-//  Écrire une fonction qui affiche toutes les différentes combinaisons de deux nombres entre 0 et 99, dans l’ordre croissant.
-// Cela donne quelque chose comme ça : $>./a.out | cat -e 00 01, 00 02, 00 03, 00 04, 00 05, ..., 00 99, 01 02, ..., 97 99, 98 99$>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c){
-    write(1,&c,1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void ft_afficher(int a,int b){
-    ft_putchar(a/10 + '0');
-    ft_putchar(a%10 + '0');
-    write(1," ",1);
-    ft_putchar(b/10 + '0');
-    ft_putchar(b%10 + '0');
-    write(1,",",1);
-    write(1," ",1);
-    
-}
+void	ft_print_comb2(void)
+{
+	int	nb1;
+	int	nb2;
 
-void ft_print_comb2(){
-    int a;
-    int b;
-    a=0;
-    b=1;
-
-    while (a<=98 && b<=99){
-        ft_afficher(a,b);
-        b++;
-        if(b==99){
-            ft_afficher(a,b);
-            a++;
-            b=a+1;
-        }
-    }
-   
-}
-
-
-
-int main(){
-    ft_print_comb2();
-    return 0;
+	nb1 = 0;
+	nb2 = 1;
+	while (nb1 <= 98)
+	{
+		while (nb2 <= 99)
+		{
+			ft_putchar(nb1 / 10 + 48);
+			ft_putchar(nb1 % 10 + 48);
+			ft_putchar(' ');
+			ft_putchar(nb2 / 10 + 48);
+			ft_putchar(nb2 % 10 + 48);
+			if (nb1 != 98)
+				write(1, ", ", 2);
+			nb2 ++;
+		}
+		nb1++;
+		nb2 = nb1 + 1;
+	}
 }
