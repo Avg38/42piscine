ccb9ed25f05a5129ccec0947baa87e068e2a7123
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:19:38 +0100

    Update 04_ft_str_is_lowercase.c

--- C02/04_ft_str_is_lowercase.c
+++ C02/04_ft_str_is_lowercase.c
@@ -1,22 +1,25 @@
-// 42
-//  04_ft_str_is_lowercase
-// Écrire une fonction qui renvoie 1 si la chaîne passée en paramètre ne contient que des caractères alphabétiques minuscules et renvoie 0 si la chaîne passée en paramètre contient d’autres types de caractères.
-// Elle devra renvoyer 1 si str est une chaîne vide.
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_str_is_numeric(char *str){
-        int i;
-        i = 0;
-        while(str[i]!='\0'){
-            if(!((str[i]>=97) && (str[i]<=122)))
-                return 0;
-            i++;
-        }
-        return 1;
-}
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
 
-int main(){
-    char str[100] = "0rv";
-    int temp = ft_str_is_numeric(str);
-    printf("%d",temp);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 'a') && (str[i] <= 'z')))
+			return (0);
+		i++;
+	}
+	return (1);
 }
