a9e5464f144eb4d5e0f5629957851a84c72ae818
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:21:38 +0100

    Update 08_ft_strlowcase.c

--- C02/08_ft_strlowcase.c
+++ C02/08_ft_strlowcase.c
@@ -1,27 +1,25 @@
-// 42
-// 08_ft_strlowcase
-// Écrire une fonction qui met en minuscule chaque lettre.
-// Elle devra renvoyer str.
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_strlowcase(char *str){
-    int i;
-    i = 0;
-    while (str[i]!='\0'){
-        if(((str[i]>= 'A') && (str[i]<='Z')))
-            str[i] = str[i] + 32;
-        i++;
-    }
-    return str;
-}
+char	*ft_strlowcase(char *str)
+{
+	int	i;
 
-int main(){
-    char str[100] = "JE SUIS TA MERE";
-    int i = 0;
-    ft_strlowcase(str);
-    while (str[i]!='\0'){
-        printf("%c",str[i]);
-        i++;
-    }
-    return 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
 }
