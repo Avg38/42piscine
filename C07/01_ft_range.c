c282b6065a8a6f9b6dc931cebd661af97635b211
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 19:59:13 +0100

    Update 01_ft_range.c

--- C07/01_ft_range.c
+++ C07/01_ft_range.c
@@ -9,5 +9,25 @@
 /*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*tab;
+	int	i;
+
+	if (min >= max)
+		return (NULL);
+	tab = (int *) malloc((sizeof(int) * (max - min)));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		min++;
+		i++;
+	}
+	return (tab);
+}
