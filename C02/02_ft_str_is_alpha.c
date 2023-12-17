c1c86f0dc9b1da8226743b48cb27f952d98d5b74
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:18:23 +0100

    Update 02_ft_str_is_alpha.c

--- C02/02_ft_str_is_alpha.c
+++ C02/02_ft_str_is_alpha.c
@@ -1,23 +1,26 @@
-//42
-//02_ft_str_is_alpha
-// Écrire une fonction qui renvoie 1 si la chaîne passée en paramètre ne contient que des caractères alphabétiques et renvoie 0 si la chaîne passée en paramètre contient d’autres types de caractères.
-// Elle devra renvoyer 1 si str est une chaîne vide.
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_str_is_alpha(char *str){
-    int i;
-    i = 0;
-    while(str[i]!='\0'){
-        if(!((str[i]>=65) && (str[i]<=90)) && ((str[i]>=97) && (str[i]<=122)))
-            return 0;
-        i++;
-    }
-    return 1;
-}
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
 
-int main(){
-    char str[100] = "Boubew";
-    int temp = ft_str_is_alpha(str);
-    printf("%d",temp);
-    return 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 65) && (str[i] <= 90)) 
+			&& !((str[i] >= 97) && (str[i] <= 122)))
+			return (0);
+		i++;
+	}
+	return (1);
 }
