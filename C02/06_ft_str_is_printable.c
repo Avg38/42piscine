917ffa38a45827ef126a285ade5efa5a26df5818
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:20:43 +0100

    Update 06_ft_str_is_printable.c

--- C02/06_ft_str_is_printable.c
+++ C02/06_ft_str_is_printable.c
@@ -1,23 +1,25 @@
-// 42
-//  06_ft_str_is_printable
-// Écrire une fonction qui renvoie 1 si la chaîne passée en paramètre ne contient que des caractères affichables et renvoie 0 si la chaîne passée en paramètre contient d’autres types de caractères.
-// Elle devra renvoyer 1 si str est une chaîne vide
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_str_is_printable(char *str){
-        int i;
-        i = 0;
-        while(str[i]!='\0'){
-            if(!(((str[i]>=32) && (str[i]<=126))))
-                return 0;
-            i++;
-        }
-        return 1;
-}
+int	ft_str_is_printable(char *str)
+{
+	int	i;
 
-int main(){
-    char str[100] ="zfg";
-    // str[4] = 28;
-    int temp = ft_str_is_printable(str);
-    printf("%d",temp);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 32) && (str[i] <= 126)))
+			return (0);
+		i++;
+	}
+	return (1);
 }
