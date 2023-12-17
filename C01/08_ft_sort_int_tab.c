1e83b9d44dc524f0209ee7feb49108000ced9f73
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:12:34 +0100

    Update 08_ft_sort_int_tab.c

--- C01/08_ft_sort_int_tab.c
+++ C01/08_ft_sort_int_tab.c
@@ -1,47 +1,31 @@
-//42
-//08_ft_sort_int_tab
-// Écrire une fonction qui trie un tableau d’entiers par ordre croissant.
-// Les paramètres sont un pointeur sur entier et le nombre d’entiers dans le tableau.
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void    ft_swap(int *a,int *b){
-    int c;
-    c = *a;
-    *a = *b;
-    *b = c;
-}
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	tmp;
+	int	i;
 
-void    ft_sort_int_tab(int *tab, int size){
-    int i;
-    i = 0;
-    size--;
-    while(i<size){
-        if(tab[i]>tab[i+1]){
-            ft_swap(&tab[i],&tab[i+1]);
-            i=-1;
-        }    
-        i++;
-    }
-}
-
-int main(){
-    int i = 0;
-    int tab[100];
-    tab[0] = 4 ;
-    tab[1] = 6 ;
-    tab[2] = 3 ;
-    tab[3] = 9 ;
-    tab[4] = 5 ;
-    tab[5] = 2 ;
-    tab[6] = 8 ;
-    tab[7] = 1 ;
-    tab[8] = 7 ;
-    tab[9] = 0 ;
-    int size = 10;
-    ft_sort_int_tab(tab,size);
-    while(i<size){
-        printf("%d",tab[i]);
-        i++;
-    }
-    return 0;
+	i = 0;
+	size--;
+	while (i < size)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = -1;
+		}
+		i++;
+	}
 }
