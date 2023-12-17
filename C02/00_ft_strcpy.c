f44d1872694d93bde336f4b3c8ee09ee3122fd4e
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:17:14 +0100

    Update 00_ft_strcpy.c

--- C02/00_ft_strcpy.c
+++ C02/00_ft_strcpy.c
@@ -1,27 +1,25 @@
-//42
-//00_ft_strcpy
-// Reproduire à l’identique le fonctionnement de la fonction strcpy (man strcpy).
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char    *ft_strcpy(char *dest, char *src){
-    int i;
-    i = 0;
-    while(src[i]!= '\0' ){
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = src[i];
-    return dest;
-}
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
 
-int main(){
-    char src[100] = "Je suis un chou-fleur";
-    char dest[100];
-    int i = 0;
-    ft_strcpy(dest,src);
-    while(dest[i]!='\0'){
-        printf("%c",dest[i]);
-        i++;
-    }
-    return 0;
+	i = 0;
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
