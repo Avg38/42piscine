8d662e6099b20087a2931446d13a25224ec70bd8
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:08:36 +0100

    Update 06_ft_find_next_prime.c

--- C05/06_ft_find_next_prime.c
+++ C05/06_ft_find_next_prime.c
@@ -9,34 +9,33 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
 
-int ft_is_prime(int nb){
-    int i;
-    i = 2;
-    while (i<nb){
-        if (nb%i == 0)
-            return 0;
-        i++;
-    }
-    return nb;
-    
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb < 2)
+		return (0);
+	if (nb % 2 == 0 && nb > 2)
+		return (0);
+	if ((nb % 5 == 0 || nb % 3 == 0) && nb > 5)
+		return (0);
+	i = 3;
+	while (i < nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
 }
 
-int ft_find_next_prime(int nb){
-    if (nb<=1)
-        return 0;
-    if (ft_is_prime(nb) == nb)
-        return nb;
-    else
-        while (ft_is_prime(nb) != nb)
-            nb++;
-    return nb;
-}
+int	ft_find_next_prime(int nb)
+{
+	int	i;
 
-int main(){
-    int res = ft_find_next_prime(9);
-    printf("%d",res);
-    return 0;
+	i = nb;
+	while (ft_is_prime(i) == 0)
+		i++;
+	return (i);
 }
