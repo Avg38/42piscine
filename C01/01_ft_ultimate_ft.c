b1082663efeaf8964e2be6249f49273738a5585e
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:08:55 +0100

    Update 01_ft_ultimate_ft.c

--- C01/01_ft_ultimate_ft.c
+++ C01/01_ft_ultimate_ft.c
@@ -1,24 +1,16 @@
-//42
-//01_ft_ultimate_ft
-// Écrire une fonction qui prend un pointeur sur pointeur sur pointeur sur pointeur sur pointeur sur pointeur sur pointeur sur pointeur sur pointeur sur int en paramètre et donne à l’int la valeur de 42.
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
-
-void    ft_ultimate_ft(int  *********nbr){
-    *********nbr=42;
-}
-
-int main(){
-    int nbr;
-    int *a = &nbr;
-    int **b = &a;
-    int ***c = &b;
-    int ****d = &c;
-    int *****e = &d;
-    int ******f = &e;
-    int *******g = &f;
-    int ********h = &g;
-    ft_ultimate_ft(&h);
-    printf("%d",nbr);
-    return 0;
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
 }
