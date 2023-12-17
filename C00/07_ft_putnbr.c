d0a3c0d52559955f2e0ba92e4caac5bbbff0a220
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:16:28 +0100

    Update 07_ft_putnbr.c

--- C00/07_ft_putnbr.c
+++ C00/07_ft_putnbr.c
@@ -1,51 +1,35 @@
-//42
-//07_ft_putnbr
-// Écrire une fonction qui affiche un nombre passé en paramètre. La fonction devra être capable d’afficher la totalité des valeurs possibles dans une variable de type int.
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c){
-    write(1,&c, 1);
-}
-
-// void ft_putnbr(int nb){
-//     int multiple;
-//     multiple=10;
-//     if(nb<0){
-//         write(1,"-",1);
-//         nb = -nb;
-//     }
-//     while(nb/multiple!=0){
-//         multiple=multiple*10;
-//     }
-//     if(nb>=100000000){
-//         multiple = 100000000;
-//         ft_putchar(nb/multiple/10 + '0');
-//     }
-//     multiple = multiple/10;
-//     while(multiple>=10){
-//         ft_putchar(nb/multiple-(nb/(multiple*10)*10)+ '0');
-//         multiple=multiple/10;
-//     }
-//     ft_putchar(nb%10 + '0');
-    
-// }
-
-void ft_putnbr(int nb){
-    if(nb<0){
-        write(1,"-",1);
-        nb=-nb;
-    }
-    if(nb>9)
-        ft_putnbr(nb/10);
-    ft_putchar(nb%10 + '0');
-}
-
-int main(){
-    ft_putnbr(-2147483647);
-    write(1,"\n",1);
-    ft_putnbr(2147483647);
-    write(1,"\n",1);
-    ft_putnbr(42);
-    return 0;
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11); 
+	else if (nb < 0)
+	{
+		nb = -nb;
+		write(1, "-", 1);
+		ft_putnbr(nb);
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb <= 9 && nb >= 0)
+	{
+		nb += 48;
+		write(1, &nb, 1);
+	}
 }
