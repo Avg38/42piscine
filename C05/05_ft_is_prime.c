1dd16d846669994ac2dc5c6e3db2874fb12f9418
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:08:14 +0100

    Update 05_ft_is_prime.c

--- C05/05_ft_is_prime.c
+++ C05/05_ft_is_prime.c
@@ -9,25 +9,23 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
 
-int ft_is_prime(int nb){
-    int i;
+int	ft_is_prime(int nb)
+{
+	int	i;
 
-    i = 2;
-    if (nb<=1)
-        return 0;
-    while (i<nb){
-        if (nb%i == 0)
-            return 0;
-        i++;
-    }
-    return 1;
-}
-
-int main(){
-    int res = ft_is_prime(3);
-    printf("%d",res);
-    return 0;
+	if (nb < 2)
+		return (0);
+	if (nb % 2 == 0 && nb > 2)
+		return (0);
+	if ((nb % 5 == 0 || nb % 3 == 0) && nb > 5)
+		return (0);
+	i = 3;
+	while (i < nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
 }
