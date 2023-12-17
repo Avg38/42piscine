a2376fd07a84e4e28071a86470aca4b7057b122c
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:06:31 +0100

    Update 00_ft_iterative_factorial.c

--- C05/00_ft_iterative_factorial.c
+++ C05/00_ft_iterative_factorial.c
@@ -9,25 +9,19 @@
 /*   Updated: 2023/07/04 19:23:04 by avg38            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
 
-int ft_iterative_factorial(int nb){
-    int i;
-    int tmp;
+int	ft_iterative_factorial(int nb)
+{
+	int	n;
 
-    i = 1;
-    tmp = 1;
-    if (nb<0)
-        return 0;    
-    while (i<=nb){
-       tmp = tmp*i++; 
-    } 
-    return tmp;
-}
-
-int main(){
-    int res = ft_iterative_factorial(-1);
-    printf("%d",res);
-    return 0;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	if (nb <= 2)
+		return (nb);
+	n = nb;
+	while (1 < --nb)
+		n = n * nb;
+	return (n);
 }
