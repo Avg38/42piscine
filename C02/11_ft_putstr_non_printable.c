d87334f7ae3efb5d4bb3895315d598e94e2f189c
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:24:15 +0100

    Update 11_ft_putstr_non_printable.c

--- C02/11_ft_putstr_non_printable.c
+++ C02/11_ft_putstr_non_printable.c
@@ -1,40 +1,40 @@
-// 42
-// 11_ft_putstr_non_printable
-// Écrire une fonction qui affiche une chaîne de caractères à l’écran. Si cette chaîne contient des caractères non-imprimables, ils devront être affichés sous forme hexadécimale (en minuscules) en les précédant d’un "backslash".
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-#include <stdio.h>
 
-void ft_putchar(char c){
-    write(1,&c,1);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void    ft_putstr_non_printable(char *str){
-    char hexa1[32] = "00000000000000001111111111111111";
-    char hexa2[32] = "0123456789abcdef0123456789abcdef";
-    int i = 0;
-    int j;
-
-    while(!(str[i]=='\0' && str[i+1]=='\0' && str[i+2]=='\0')){
-        j = str[i];
-        if(j<31){
-            write(1,"\\",1);
-            ft_putchar(hexa1[j]);
-            ft_putchar(hexa2[j]);
-        }else{
-            ft_putchar(str[i]);
-        }
-        i++;
-    }
-
-    
-
+void	ft_convert(unsigned char c)
+{
+	write(1, "\\", 1);
+	ft_putchar("0123456789abcdef"[c / 16]);
+	ft_putchar("0123456789abcdef"[c % 16]);
 }
 
-int main(){
-    char str[30] = "Coucou\ntu vas bien?";
-    int i = 0;
-    ft_putstr_non_printable(str);
-    
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
 
-    return 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 32 || str[i] > 126)
+			ft_convert((unsigned char)str[i]);
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
 }
