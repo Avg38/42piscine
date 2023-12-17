9ea6266319ca82be3ed14e8deecb660049b693d6
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:22:11 +0100

    Update 09_ft_strcapitalize.c

--- C02/09_ft_strcapitalize.c
+++ C02/09_ft_strcapitalize.c
@@ -1,39 +1,41 @@
-// 42
-// 09_ft_strcapitalize
-// Écrire une fonction qui met en majuscule la première lettre de chaque mot et le reste du mot en minuscule.
-// Un mot est une suite de caractères alphanumériques.
-// Elle devra renvoyer str
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_strcapitalize(char *str){
-    int i;
-    i = 1;
-    if((str[0]>='a' && str[0]<='z')){
-        str[0] = str[0] - 32;
-    }
-    while(str[i]!='\0'){
-        if((str[i]>='a' && str[i]<='z')){
-            if(!(((str[i-1]>='A') && (str[i-1]<='Z')) || ((str[i-1]>='a') && (str[i-1]<='z')) || ((str[i-1]>='0') && (str[i-1]<='9')))){
-                str[i] = str[i] - 32;
-            }
-        }
-        if((str[i]>='A' && str[i]<='Z')){
-            if((((str[i-1]>='A') && (str[i-1]<='Z')) || ((str[i-1]>='a') && (str[i-1]<='z')) || ((str[i-1]>='0') && (str[i-1]<='9')))){
-                str[i] = str[i] + 32;
-            }
-        }
-        i++;
-    }
-    return str;
+int	check_is_start(char c, char before, int i)
+{
+	if (before < '0' || (before > '9' && before < 'A') 
+		|| (before > 'Z' && before < 'a') 
+		|| before > 'z' || i == 0)
+	{
+		if (c >= 'a' && c <= 'z')
+			return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+		return (2);
+	return (0);
 }
 
-int main(){
-    char str[100] = "salut, coMment tu vas ? 42Mots quarante-deux; cinquante+et+un";
-    int i = 0;
-    ft_strcapitalize(str);
-    while (str[i]!='\0'){
-        printf("%c",str[i]);
-        i++;
-    }  
-    return 0;
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (check_is_start(str[i], str[i - 1], i) == 1)
+			str[i] -= 32;
+		if (check_is_start(str[i], str[i - 1], i) == 2)
+			str[i] += 32;
+		i++;
+	}
+	return (str);
 }
