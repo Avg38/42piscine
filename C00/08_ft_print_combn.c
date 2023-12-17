d26cfb766084fc26a0684cbb3c0cb2061d40ca01
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:16:58 +0100

    Update 08_ft_print_combn.c

--- C00/08_ft_print_combn.c
+++ C00/08_ft_print_combn.c
@@ -1,60 +1,76 @@
-//42
-//08_ft_printcombn
-// Écrire une fonction qui affiche toutes les différentes combinaisons de n chiffres dans l’ordre croissant.
-// • n sera tel que : 0 < n < 10.
-// • Si n = 2, cela donne quelque chose comme ça : $>./a.out | cat -e 01, 02, 03, ..., 09, 12, ..., 79, 89$>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c){
-    write(1,&c,1);
+void	ft_putnbr(int *tab, int *max, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		tab[i] += 48; 
+		write(1, &tab[i], 1);
+		tab[i++] -= 48;
+	}
+	if (tab[0] != max [0])
+		write(1, ", ", 2);
 }
 
-void ft_print_combn(int n){
-    int nb[n]; //Tableau qui stockera les différentes combinaisons de n chiffres
-    int nbLast[n]; //Tableau qui stocke la dernière combinaison
-    int i; //Variable qui permettra d'incrémenter les valeur du tableau
-    int j; //Idem
-    i = 0;
-    j = 9;
-    while(i!=n){ //Inititalisation du tableau nb[] à la première valeur souhaitée selon n
-        nb[i]=i;
-        i++;
-    }
-    i = n-1; //n-1 car le tableau va de 0 à n-1
-    while(i>=0){ //Initialisation de la dernière combinaison selon n
-        nbLast[i] = j;
-        j--;
-        i--;
-    }
-    
-    while(nb[0]!=nbLast[0] && nb[n-1]<=nbLast[n-1]){ //Boucle qui va incrémenter les valeur tout en vérifiant leur validité
-        j = 0;
-        while(j<=n-1){
-            ft_putchar(nb[j]+'0'); //On affiche les valeurs de nb[]
-            j++;
-        }
-        ft_putchar(44); //virgule en code ASCII
-        ft_putchar(32); //espace en code ASCII
-        
-        i=n-1;
-        nb[i]=nb[i]+1; //On incrémente l'unité
-        if(nb[i]==10){ //Dès que l'unité arrive à 10 
-            while(i>=0){
-                nb[i-1] = nb[i-1]+1; //On augmente la valeur d'avant de 1, puis la valeur d'avant, etc...
-                i--;
-            }
-            nb[n-1] = nb[n-2]+1; //On met l'unité à la valeur de la case précédente +1
-        } 
-    }
-    i=0;
-    while(i<n){
-        ft_putchar(nbLast[i] + '0'); //On affiche le dernier caractère pour ne pas avoir la virgule à la fin
-        i++;
-    }
+int	init_max(int *max, int n)
+{
+	int	i;
+	int	nb;
+
+	nb = 9;
+	i = n - 1;
+	while (0 <= i)
+		max[i--] = nb--;
+	return (*max);
 }
 
-int main(){
-    ft_print_combn(4);
-    return 0;
+void	ft_run(int *max, int *tab, int n)
+{
+	int	i;
+
+	i = n - 1;
+	ft_putnbr(tab, max, n);
+	while (tab[0] != max[0])
+	{
+		while (tab[i] == max[i])
+			i--;
+		tab[i]++;
+		while (i < n - 1)
+		{
+			i++;
+			tab[i] = tab[i - 1] + 1;
+		}
+		ft_putnbr(tab, max, n);
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	max[10];
+	int	tab[10];
+	int	i;
+	int	nb;
+
+	i = 0;
+	nb = 0;
+	while (i < n)
+		tab[i++] = nb++;
+	tab[i] = '\0';
+	init_max(max, n);
+	ft_run(max, tab, n);
 }
