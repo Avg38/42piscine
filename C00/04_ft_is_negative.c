3b391c31f5b009752d3a2f1ccaa50e861da48eae
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:14:55 +0100

    Update 04_ft_is_negative.c

--- C00/04_ft_is_negative.c
+++ C00/04_ft_is_negative.c
@@ -1,19 +1,26 @@
-//42
-//04_ft_is_negative
-// Écrire une fonction qui affiche ’N’ ou ’P’ suivant le signe de l’entier passé en paramètre. Si n est négatif alors afficher ’N’. Si n est positif ou nul alors afficher ’P’.
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_is_negative(int n){
-    int negatif = 'N';
-    int positif = 'P';
-    if(n>=0){
-        write(1, &positif, 1);
-    } else{
-        write(1, &negatif, 1);
-    }
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-int main(){
-    ft_print_comb2();
-    return 0;
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
 }
