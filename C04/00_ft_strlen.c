fbe661dd77fe3c97c29902b602a7f251ea27fcf6
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:09:37 +0100

    Update 00_ft_strlen.c

--- C04/00_ft_strlen.c
+++ C04/00_ft_strlen.c
@@ -9,20 +9,14 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
 
-int ft_strlen(char *str){
+int    ft_strlen(char *str){
     int i;
+    
     i = 0;
-    while (str[i]){
+    while (str[i])
         i++;
-    }
-    return i--;
-}
-
-int main(){
-    char str[100] = "Je suis une banane";
-    int res = ft_strlen(str);
-    printf("%d",res);
-    return 0;
+    return (i);
 }
