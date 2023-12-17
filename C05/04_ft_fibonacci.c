ea34a3199d10465c92ac4aacca3ec374a820e29f
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:07:45 +0100

    Update 04_ft_fibonacci.c

--- C05/04_ft_fibonacci.c
+++ C05/04_ft_fibonacci.c
@@ -9,27 +9,14 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
 
-int run(int i, int nb1, int nb2, int index){
-    if (i<index)
-        nb2 = run(i+1,nb2,nb2+nb1,index);
-    return nb2;
-}
-
-int ft_fibonacci(int index){
-    
-    int res;
-
-    if (index<0)
-        return -1;   
-    res = run(2,0,1,index);
-    return res;
-}
-
-int main(){
-    int res = ft_fibonacci(10);
-    printf("%d",res);
-    return 0;
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
