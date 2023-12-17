3e011a6510c754e23ec37a4fec6062f2212bc98e
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:15:26 +0100

    Update 05_ft_print_comb.c

--- C00/05_ft_print_comb.c
+++ C00/05_ft_print_comb.c
@@ -1,38 +1,49 @@
-//42
-//05_ft_print_comb
-// Écrire une fonction qui affiche, dans l’ordre croissant, toutes les différentes combinaisons de trois chiffres différents dans l’ordre croissant - oui, la répétition est volontaire.
-// Cela donne quelque chose comme ça : $>./a.out | cat -e 012, 013, 014, 015, 016, 017, 018, 019, 023, ..., 789$> 
-// • 987 n’est pas là car 789 est déjà présent
-// • 999 n’est pas là car ce nombre ne comporte pas exclusivement des chiffres différents les uns des autres
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_comb(){
-    char a;
-    char b;
-    char c;
-    char d;
-    a=48;
-    b=49;
-    c=50;
-    while(a<56){
-        write(1,&a,1);
-        write(1,&b,1);
-        write(1,&c,1);
-        c++;
-        if(c>57){
-            b++;
-            c=b+1;
-        }
-        if(b>56){
-            a++;
-            b=a+1;
-            c=b+1;
-        }
-        write(1," ",1);
-    }
+void	ft_print_numbers(int nb1, int nb2, int nb3)
+{
+	write(1, &nb1, 1);
+	write(1, &nb2, 1);
+	write(1, &nb3, 1);
+	if (nb1 != '7')
+		write(1, ", ", 2);
 }
 
-int main(){
-    ft_print_comb2();
-    return 0;
+void	ft_print_comb(void)
+{
+	int	nb1;
+	int	nb2;
+	int	nb3;
+
+	nb1 = 48;
+	nb2 = 48 + 1;
+	nb3 = 48 + 2;
+	while (nb1 <= '7')
+	{
+		while (nb2 <= '8')
+		{
+			while (nb3 <= '9')
+			{
+				ft_print_numbers(nb1, nb2, nb3);
+				nb3++;
+			}
+			nb2++;
+			nb3 = nb2 + 1;
+		}
+		nb1++;
+		nb2 = nb1 + 1;
+		nb3 = nb2 + 1;
+	}
 }
