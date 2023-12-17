eb6095cdd71e068d46bba9d814d206f215a14c60
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 22:12:05 +0100

    Update 07_ft_rev_int_tab.c

--- C01/07_ft_rev_int_tab.c
+++ C01/07_ft_rev_int_tab.c
@@ -1,36 +1,27 @@
-//42
-//07_ft_rev_int_tab
-// Écrire une fonction qui inverse l’ordre des élements d’un tableau d’entiers.
-// Les paramètres sont un pointeur sur entier et le nombre d’entiers dans le tableau.
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
 
-void    ft_rev_int_tab(int *tab, int size){
-    int tabtwo[100]; //Création d'un deuxième tableau mais j'aurai pu mettre une variable temporaire qui stocke les valeur du tableau au fur et à mesure
-    int i;
-    i = 0;
-    while(i!=size){
-        tabtwo[i]=tab[i];
-        i++;
-    }
-    i = 0 ;
-    int j = 4;
-    while(i!=j){
-        tab[i] = tabtwo[size-1];
-        printf("%d",tab[i]);
-        i++;
-        size--;
-    }
-}
-
-int main(){
-    int tab[100];
-    tab[0] = 0;
-    tab[1] = 1;
-    tab[2] = 2;
-    tab[3] = 3;
-    int size = 4;
-    ft_rev_int_tab(tab,size);
-
-    return 0;
+	i = 0;
+	j = size - 1;
+	while (i < j)
+	{
+		tmp = tab[i];
+		tab[i++] = tab[j];
+		tab[j--] = tmp;
+	}
 }
