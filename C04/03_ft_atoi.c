ffb907566f2d90fa205f137b9eaff65d9dad9e31
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:11:35 +0100

    Update 03_ft_atoi.c

--- C04/03_ft_atoi.c
+++ C04/03_ft_atoi.c
@@ -9,44 +9,30 @@
 /*   Updated: 2019/08/04 19:23:04 by ybayart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
 
-void ft_putchar(char c){
-    write(1,&c,1);
-}
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	nb;
 
-int ft_atoi(char *str){
-    int count;
-    count = 0;
-    int tmp;
-    tmp = 0;
-    int i;
-    i = 0;
-    int nb;
-    nb = 0;
-    while (str[i]!='\0'){
-        if(str[i] == '-' && tmp != 1)
-            count++;
-        if(str[i] >= '0' && str[i] <= '9'){
-            tmp = 1; 
-            if(nb > 0)
-                nb = nb*10 + str[i] - '0';
-            else
-                nb = str[i] - '0';
-        }else if(!(str[i] >= '0' && str[i] <= '9') && tmp == 1){
-            if(count%2 != 0)
-                nb = -nb;
-            return nb;
-        }
-        i++;
-    } 
-}
-
-int main(){
-    int i = 0;
-    char str[100] = "  -++-5356az-gnh245";
-    int res = ft_atoi(str);
-    printf("%d",res);
-    return 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	nb = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (nb != 0)
+			nb *= 10;
+		nb += str[i] - 48;
+		i++;
+	}
+	return (nb * sign);
 }
