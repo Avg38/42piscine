22959acb2afbfbcc2c73fe0c5391ae6eb2e4a4de
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 20:24:50 +0100

    Update 12_ft_print_memory.c

--- C02/12_ft_print_memory.c
+++ C02/12_ft_print_memory.c
@@ -1,29 +1,110 @@
-// 42
-// 12_ft_print_memory
-// Écrire une fonction qui affiche une zone mémoire à l’écran.
-// • L’affichage de la zone mémoire est séparée en trois "colonnes" séparées par un espace :
-//  ◦ L’adresse en hexadécimal du premier caractère de la ligne suivi d’un ’ :’.
-//  ◦ Le contenu en hexadécimal avec un espace tous les deux caractères et doit etre complété avec des espaces si nécessaire (voir l’exemple en dessous).
-//  ◦ Le contenu en caractères imprimables.
-// • Si un caractère est non-imprimable il sera remplacé par un point.
-// • Chaque ligne doit gérer seize caractères.
-// • Si size est égale à 0, rien ne sera affiché.
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_memory.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avg38                                    +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 18:18:01 by avg38             #+#    #+#             */
+/*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-
-
-int main()
+void	ft_putchar(char c)
 {
-    char c = 'c';
+	write(1, &c, 1);
+}
 
-    char *ptr = &c;
-    int addr = (int*)ptr;
+void	print_address(unsigned long long addr)
+{
+	char	stack[16];
+	char	*base;
+	int		i;
 
-    printf("addr c = %p\n", (int*)ptr);
-    printf("addr ptr = %p\n", (void*)&c);
-    printf("addr = %d", addr);
+	base = "0123456789abcdef";
+	i = 0;
+	while (i < 16)
+	{
+		stack[i] = base[addr % 16];
+		i++;
+		addr /= 16;
+	}
+	i = 15;
+	while (i >= 0)
+	{
+		ft_putchar(stack[i]);
+		i--;
+	}
+	write(1, ": ", 2);
+}
 
+void	print_hexa(unsigned char *p, int size)
+{
+	char	*base;
+	int		i;
 
-    return 0;
+	i = 0;
+	base = "0123456789abcdef";
+	while (i < size)
+	{
+		ft_putchar(base[p[i] / 16]);
+		ft_putchar(base[p[i] % 16]);
+		if (i % 2 == 1)
+			ft_putchar(' ');
+		i++;
+	}
+	while (i < 16)
+	{
+		i++;
+		write(1, "  ", 2);
+		if (i % 2 == 1)
+			ft_putchar(' ');
+	}
+	if (size % 2 == 1)
+		ft_putchar(' ');
+}
+
+void	print_text(unsigned char *p, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		if (p[j] >= 32 && p[j] <= 126) 
+			ft_putchar(p[j]);
+		else
+			ft_putchar('.');
+		j++;
+	}
+	ft_putchar('\n');
+}
+
+void	*ft_print_memory(void *addr, unsigned int size)
+{
+	unsigned char	*p;
+	unsigned int	offset;
+
+	p = (unsigned char *)addr;
+	offset = 0;
+	while (size > 0)
+	{
+		print_address((unsigned long long)&p[offset]);
+		if (size >= 16)
+		{
+			print_hexa(&p[offset], 16);
+			print_text(&p[offset], 16);
+			offset += 16;
+			size -= 16;
+		}
+		else
+		{
+			print_hexa(&p[offset], size);
+			print_text(&p[offset], size);
+			size = 0;
+		}
+	}
+	return (addr);
 }
