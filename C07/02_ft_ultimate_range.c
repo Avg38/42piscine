a9d621a338642911a4830aa6b44d756c1771c035
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-17 19:59:30 +0100

    Update 02_ft_ultimate_range.c

--- C07/02_ft_ultimate_range.c
+++ C07/02_ft_ultimate_range.c
@@ -9,5 +9,29 @@
 /*   Updated: 2023/07/05 19:23:04 by avg38            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <unistd.h>
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+	int	*tab;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	tab = (int *)malloc(sizeof(int) * (max - min));
+	if (range == NULL)
+		return (-1);
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	*range = tab;
+	return (i);
+}
